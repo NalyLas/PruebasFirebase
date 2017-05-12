0df6bbaa8dfd00996681f4db1a8f954ade937923
ptmarketing04 <uweb@megasur.es>
2017-05-12 17:03:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ptmarketing04/pruebasfirebase/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.ptmarketing04.pruebasfirebase;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOGTAG = "android-fcm";
+
+    private Button btnToken;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        //Probar el archivo que has subido a drive en casa en servidor local
+        //no funciona en el servidor remoto del ayala
+        //puede que por el servidor prueba desde casa si te genera la notificacion.
+
+        btnToken = (Button)findViewById(R.id.btnToken);
+        btnToken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Se obtiene el token actualizado
+                String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+                Log.d(LOGTAG, "Token actualizado: " + refreshedToken);
+            }
+        });
+    }
+}
